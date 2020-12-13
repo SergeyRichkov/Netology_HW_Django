6ddcd559b08c8da89b5c75207ff0f06eecf3e13f
Sergey Richkov <sergey.rychkov.2016@gmail.com>
2020-12-13 12:17:01 +0700

    for_check

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -1,17 +1,11 @@
-from django.views.generic import ListView
 from django.shortcuts import render
-
 from .models import Student
 
 
 def students_list(request):
     template = 'school/students_list.html'
 
-    students_list = Student.objects.all().prefetch_related('teacher')
-
-    for a in students_list:
-        print(a.teacher.all())
-
+    students_list = Student.objects.all().prefetch_related('teacher').order_by('name')
 
     context = {
         'students_list': students_list,
